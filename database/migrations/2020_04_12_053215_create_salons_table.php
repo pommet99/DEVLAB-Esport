a16b2de5ba18b2ee8c1cfa74fba1919c362b6156
Prameet <47789905+Prameet@users.noreply.github.com>
2020-05-12 18:08:44 +0200

    added base

---
+++ database/migrations/2020_04_12_053215_create_salons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+			$table->string('name')->unique();
+			$table->string('password')->nullable();
+			$table->string('status');
+			$table->integer('nombre_de_joueur');
+			$table->boolean('public_private');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salons');
+    }
+}
